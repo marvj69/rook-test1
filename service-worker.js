c070c914ed5313ad47fc1f5252c548316fce080e
Mark Heinonen <heinonenmh@gmail.com>
2025-03-05 15:32:48 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'realtor-finance-tracker-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json',
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.7.0/chart.min.js'
+];
+
+// Install event – cache all necessary files
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log('Opened cache');
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch event – serve cached content when offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
